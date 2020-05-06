8c0e236b8ca5b2b18bdd1719d991e0be7869470b
Ryan Lin Xiang <Ryan.Lin.Xiang@gmail.com>
2020-05-06 10:33:01 +0200

    Layout bugfixes

--- public/script_main.js
+++ public/script_main.js
@@ -57,9 +57,10 @@ function create_items(items, mode) {
 function create_itemfull(item) {
     console.log(item.description);
         div_itemfull.find(".modal-content").empty();
-        div_itemfull.find(".modal-card-title").empty();
-        div_itemfull.find(".modal-card-title").append( `${item.title}` );          
-        div_itemfull.find(".modal-content").append( `<p>${item.description}</p><div class="content is-small">posted on ${item.creation_date.split("-")[2].split("T")[0]}/${item.creation_date.split("-")[1]}/${item.creation_date.split("-")[0]} (${item.dayspast} day${item.dayspast>1?'s':''} old)</div>`);    
+        div_itemfull.find(".modal-content").append( `<div class="content"><p class="title is-4 has-text-white">${item.title}</p>
+                                                     <p class="subtitle has-text-white">${item.description}</p>
+                                                     <p class="content is-small has-text-white">posted on ${item.creation_date.split("-")[2].split("T")[0]}/${item.creation_date.split("-")[1]}/${item.creation_date.split("-")[0]} (${item.dayspast} day${item.dayspast>1?'s':''} old)</p>
+                                                     </div>` );    
         div_itemfull.toggleClass( "is-active" );              
 }
 
