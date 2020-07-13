65e0526e11eb39445a3ada0c06281213f789be79
Ryan Lin Xiang <Ryan.Lin.Xiang@gmail.com>
2020-07-13 15:20:25 +0200

    added screenshot

--- api.js
+++ api.js
@@ -1,15 +1,15 @@
 // using node.js and express.js
 // initializing express.js and mariadb
-const express = require('express');
-const mariadb = require('mariadb');
-const bodyParser =  require('body-parser');
+const express = require("express");
+const mariadb = require("mariadb");
+const bodyParser = require("body-parser");
 
 // mariadb connector
 let connection_id = mariadb.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'kleinanzeigen'
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "superlist",
 });
 
 // initialize constant app with express.js, set server port to 3000
@@ -20,108 +20,128 @@ app.use(bodyParser.json());
 app.use(express.static("public"));
 
 // routing for index.html
-app.get('/', (req, res) => {
-    const path = require('path');
-    res.sendFile(path.resolve(__dirname, 'public/index.html'));
+app.get("/", (req, res) => {
+  const path = require("path");
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
 });
 
 // routing show all entries
-app.get('/show', (req, res) => {
-    let apiData = [];
-    apiData.push([]);
-    apiData.push([]);
-    
-    // asynchronous function
-    connection_id.then(async kleinanzeigen => {
-        // query select all location names once from database
-        await kleinanzeigen.query(
-                'SELECT DISTINCT location FROM anzeigen WHERE creation_date > DATE_SUB(NOW(), INTERVAL 2 WEEK)',
-            )
-            // push data in apiData array at index 0
-            .then(currentValue => {
-                currentValue.forEach(element => {
-                    apiData[0].push(element.location);
-                });
-            });
+app.get("/show", (req, res) => {
+  let apiData = [];
+  apiData.push([]);
+  apiData.push([]);
 
-        // query select top 5 locations in descending order
-        await kleinanzeigen.query(
-                'SELECT location, count(*) nums FROM anzeigen WHERE creation_date > DATE_SUB(NOW(), INTERVAL 2 WEEK) GROUP BY location ORDER BY nums DESC LIMIT 5',
-            )
-            // push data in apiData at index 1
-            .then(currentValue => {
-                currentValue.forEach(element => {
-                    apiData[1].push(element.location);
-                });
-            });
+  // asynchronous function
+  connection_id.then(async (superlist) => {
+    // query select all location names once from database
+    await superlist
+      .query(
+        "SELECT DISTINCT location FROM ads WHERE creation_date > DATE_SUB(NOW(), INTERVAL 2 WEEK)"
+      )
+      // push data in apiData array at index 0
+      .then((currentValue) => {
+        currentValue.forEach((element) => {
+          apiData[0].push(element.location);
+        });
+      });
 
-        // query select 20 entries in descending order not older than 2 weeks
-        await kleinanzeigen.query(
-                'SELECT id, title, description FROM anzeigen WHERE creation_date > DATE_SUB(NOW(), INTERVAL 2 WEEK) ORDER BY creation_date DESC LIMIT 20',
-            )
-            // push date in apiData at index 2
-            .then(currentValue => {
-                currentValue.forEach(element => {
-                    apiData.push(element);
-                });
-            });
-        res.json(apiData);
-    });
-})
+    // query select top 5 locations in descending order
+    await superlist
+      .query(
+        "SELECT location, count(*) nums FROM ads WHERE creation_date > DATE_SUB(NOW(), INTERVAL 2 WEEK) GROUP BY location ORDER BY nums DESC LIMIT 5"
+      )
+      // push data in apiData at index 1
+      .then((currentValue) => {
+        currentValue.forEach((element) => {
+          apiData[1].push(element.location);
+        });
+      });
+
+    // query select 20 entries in descending order not older than 2 weeks
+    await superlist
+      .query(
+        "SELECT id, title, description FROM ads WHERE creation_date > DATE_SUB(NOW(), INTERVAL 2 WEEK) ORDER BY creation_date DESC LIMIT 20"
+      )
+      // push date in apiData at index 2
+      .then((currentValue) => {
+        currentValue.forEach((element) => {
+          apiData.push(element);
+        });
+      });
+
+    res.json(apiData);
+  });
+});
 
 // routing show id
-app.get('/show/:id', (req, res) => {
-    connection_id.then(connection => {
-        connection.query(
-            'SELECT *, DATEDIFF(CURRENT_TIMESTAMP , creation_date) AS dayspast FROM anzeigen WHERE id = ?',
-            req.params.id
-        )
-        .then(currentValue => res.json(currentValue[0]));
-    });
-})
+app.get("/show/:id", (req, res) => {
+  connection_id.then((connection) => {
+    connection
+      .query(
+        "SELECT *, DATEDIFF(CURRENT_TIMESTAMP , creation_date) AS dayspast FROM ads WHERE id = ?",
+        req.params.id
+      )
+      .then((currentValue) => res.json(currentValue[0]));
+  });
+});
 
 // routing search keyword in title
-app.get('/search/:keyword', (req, res) => {
-    connection_id.then(connection => {
-        connection.query(
-            'SELECT id, title, description FROM anzeigen WHERE (LOWER (title) LIKE LOWER (?) OR LOWER (title) LIKE LOWER (?) OR LOWER (title) LIKE LOWER (?)) AND creation_date > DATE_SUB(NOW(), INTERVAL 2 WEEK) ORDER BY creation_date DESC LIMIT 20',
-            [req.params.keyword + '%', '%' + req.params.keyword + '%', '%' + req.params.keyword]
-        )
-        .then(currentValue => res.json(currentValue));
-    });
-})
+app.get("/search/:keyword", (req, res) => {
+  connection_id.then((connection) => {
+    connection
+      .query(
+        "SELECT id, title, description FROM ads WHERE (LOWER (title) LIKE LOWER (?) OR LOWER (title) LIKE LOWER (?) OR LOWER (title) LIKE LOWER (?)) AND creation_date > DATE_SUB(NOW(), INTERVAL 2 WEEK) ORDER BY creation_date DESC LIMIT 20",
+        [
+          req.params.keyword + "%",
+          "%" + req.params.keyword + "%",
+          "%" + req.params.keyword,
+        ]
+      )
+      .then((currentValue) => res.json(currentValue));
+  });
+});
 
 // routing search city
-app.get('/search/loc/:city', (req, res) => {
-    connection_id.then(connection => {
-        connection.query(
-            'SELECT id, title, description, location, creation_date FROM anzeigen WHERE location = ? AND creation_date > DATE_SUB(NOW(), INTERVAL 2 WEEK) ORDER BY creation_date DESC LIMIT 20',
-            req.params.city
-        )
-        .then(currentValue => res.json(currentValue));
-    });
-})
+app.get("/search/loc/:city", (req, res) => {
+  connection_id.then((connection) => {
+    connection
+      .query(
+        "SELECT id, title, description, location, creation_date FROM ads WHERE location = ? AND creation_date > DATE_SUB(NOW(), INTERVAL 2 WEEK) ORDER BY creation_date DESC LIMIT 20",
+        req.params.city
+      )
+      .then((currentValue) => res.json(currentValue));
+  });
+});
 
 // routing add new item
-app.post('/add', (req, res) => {
-    const newItem = {
-        title: req.body.title,
-        description: req.body.description,
-        name: req.body.name,
-        location: req.body.loc,
-        price: req.body.price,
-        vb: req.body.vb,
-        email: req.body.email
-    };
+app.post("/add", (req, res) => {
+  const newItem = {
+    title: req.body.title,
+    description: req.body.description,
+    name: req.body.name,
+    location: req.body.loc,
+    price: req.body.price,
+    vb: req.body.vb,
+    email: req.body.email,
+  };
 
-    connection_id.then(connection => {   
-        connection.query(
-            'INSERT INTO anzeigen (title, description, name, location, price, vb, email, creation_date) VALUES (?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)',
-            [newItem.title, newItem.description, newItem.name, newItem.location, newItem.price, newItem.vb, newItem.email]
-        )
-        .then(console.log)
-        .catch(console.log)
-    });
-})
+  connection_id.then((connection) => {
+    connection
+      .query(
+        "INSERT INTO ads (title, description, name, location, price, vb, email, creation_date) VALUES (?, ?, ?, ?, ?, ?, ?, CURRENT_TIMESTAMP)",
+        [
+          newItem.title,
+          newItem.description,
+          newItem.name,
+          newItem.location,
+          newItem.price,
+          newItem.vb,
+          newItem.email,
+        ]
+      )
+      .then(console.log)
+      .catch(console.log);
+  });
+});
 
 app.listen(port, () => console.log(`Server is running... Port: ${port}`));
--- public/script_main.js
+++ public/script_main.js
@@ -1,5 +1,3 @@
-
-
 let div_items = $("#items");
 let div_top5 = $("#top5");
 let div_itemfull = $("#itemfull");
@@ -22,48 +20,63 @@ let lastclass;
 let url = "http://localhost:3000/";
 let data = {};
 let error = false;
-  
 
 function connect_to_api(url, type, data) {
-    return new Promise((resolve,reject)=>{   
-        $.ajax({
-            url: url,
-            contentType: "application/json",
-            type: type,
-            data: data,
-            success: resolve,
-            error: reject
-        });
-    })
-}   
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: url,
+      contentType: "application/json",
+      type: type,
+      data: data,
+      success: resolve,
+      error: reject,
+    });
+  });
+}
 
 function create_items(items, mode) {
-    
-    div_items.empty();
+  div_items.empty();
 
-    items.forEach((e, i) => {
-        if (i === 0 && mode == "default")
-            e.forEach(e => { all_locs.push(e) });
-        else if (i === 1 && mode == "default") {
-            div_top5.find("ul").empty();
-            e.forEach(e => { div_top5.find("ul").append(`<li><a data-loc="${e}" href="javascript:void(0);" class="search_locs">${e}</a></li>`) });
-        }    
-        else
-            div_items.append(`<div class="box"><p class="title is-4">${e.title}</p><p class="subtitle">${e.description.substring(0, 99)} ...<a href="javascript:void(0);" class="readmore" data-id="${e.id}">read more</a></p></div>`);
-    })
-
+  items.forEach((e, i) => {
+    if (i === 0 && mode == "default")
+      e.forEach((e) => {
+        all_locs.push(e);
+      });
+    else if (i === 1 && mode == "default") {
+      div_top5.find("ul").empty();
+      e.forEach((e) => {
+        div_top5
+          .find("ul")
+          .append(
+            `<li><a data-loc="${e}" href="javascript:void(0);" class="search_locs">${e}</a></li>`
+          );
+      });
+    } else
+      div_items.append(
+        `<div class="box"><p class="title is-4">${
+          e.title
+        }</p><p class="subtitle">${e.description.substring(
+          0,
+          99
+        )} ...<a href="javascript:void(0);" class="readmore" data-id="${
+          e.id
+        }">read more</a></p></div>`
+      );
+  });
 }
 
 function create_itemfull(item) {
+  div_itemfull.find(".modal-card-title").empty();
+  div_itemfull.find(".modal-card-title").append(`${item.title}`);
+  div_itemfull.find(".modal-card-body").empty();
+  div_itemfull
+    .find(".modal-card-body")
+    .append(`<p class="subtitle">${item.description}</p>`);
+  div_itemfull.find(".modal-card-foot").empty();
+  // div_itemfull.find(".modal-card-foot").append(`<p class="content">${item.price} Euro, ${item.vb?'negotiable':'not negotiable'} posted on ${item.creation_date.split("-")[2].split("T")[0]}/${item.creation_date.split("-")[1]}/${item.creation_date.split("-")[0]} (${item.dayspast} day${item.dayspast>1?'s':''} old) by <a href="mailto:${item.email}">${item.name}</a></p>`);
 
-        div_itemfull.find(".modal-card-title").empty();
-        div_itemfull.find(".modal-card-title").append(`${item.title}`);
-        div_itemfull.find(".modal-card-body").empty();
-        div_itemfull.find(".modal-card-body").append( `<p class="subtitle">${item.description}</p>`);    
-        div_itemfull.find(".modal-card-foot").empty();    
-       // div_itemfull.find(".modal-card-foot").append(`<p class="content">${item.price} Euro, ${item.vb?'negotiable':'not negotiable'} posted on ${item.creation_date.split("-")[2].split("T")[0]}/${item.creation_date.split("-")[1]}/${item.creation_date.split("-")[0]} (${item.dayspast} day${item.dayspast>1?'s':''} old) by <a href="mailto:${item.email}">${item.name}</a></p>`);                                        
-    
-        div_itemfull.find(".modal-card-foot").append(`<nav class="level" style="width:100%">
+  div_itemfull.find(".modal-card-foot")
+    .append(`<nav class="level" style="width:100%">
         <div class="level-item has-text-centered">
           <div>
             <p class="heading">Loc</p>
@@ -79,7 +92,7 @@ function create_itemfull(item) {
         <div class="level-item has-text-centered">
           <div>
             <p class="heading">Negotiable?</p>
-            <p class="title">${item.vb?'yes':'no'}</p>
+            <p class="title">${item.vb ? "yes" : "no"}</p>
           </div>
         </div>
         <div class="level-item has-text-centered">
@@ -94,106 +107,132 @@ function create_itemfull(item) {
             <p class="title">${item.dayspast}</p>
           </div>
         </div>
-      </nav>`); 
+      </nav>`);
 
-        div_itemfull.toggleClass( "is-active" );              
+  div_itemfull.toggleClass("is-active");
 }
 
 function create_matches(key) {
-    let matches;
-    ul_matches.empty();
-    let regexp = new RegExp(`${key}`,"i");
-    if (inp_search_loc.val()) {
-        if (matches = all_locs.filter(e=>e.search(regexp)!=-1?1:0)) {
-        matches.forEach(e=>ul_matches.append(`<li><a href="javascript:void(0)" class="dropdown-item">${e}</a></li>`));
-        }
-    }    
+  let matches;
+  ul_matches.empty();
+  let regexp = new RegExp(`${key}`, "i");
+  if (inp_search_loc.val()) {
+    if ((matches = all_locs.filter((e) => (e.search(regexp) != -1 ? 1 : 0)))) {
+      matches.forEach((e) =>
+        ul_matches.append(
+          `<li><a href="javascript:void(0)" class="dropdown-item">${e}</a></li>`
+        )
+      );
+    }
+  }
 }
 
-connect_to_api(url+"show", "GET").then(e=>create_items(e,"default"));
+connect_to_api(url + "show", "GET").then((e) => create_items(e, "default"));
 
-div_items.on("click",e=>{
-    if(e.target.className==="readmore") 
-        connect_to_api(url+"show/"+e.target.dataset.id, "GET").then(create_itemfull);    
+div_items.on("click", (e) => {
+  if (e.target.className === "readmore")
+    connect_to_api(url + "show/" + e.target.dataset.id, "GET").then(
+      create_itemfull
+    );
 });
 
-div_top5.on("click",e=>{
-    if(e.target.className==="search_locs") 
-        connect_to_api(url+"search/loc/"+e.target.dataset.loc, "GET").then(e=>create_items(e,"search"));        
+div_top5.on("click", (e) => {
+  if (e.target.className === "search_locs")
+    connect_to_api(
+      url + "search/loc/" + e.target.dataset.loc,
+      "GET"
+    ).then((e) => create_items(e, "search"));
 });
 
-btn_search_submit.on("click",()=>{
-    if (inp_search.val()) { 
-        connect_to_api(url+"search/"+inp_search.val(), "GET").then(e=>create_items(e,"search"));
-    }
-})
-
-
-
-form.on("submit", e=>{
- 
-    e.preventDefault();
-    let root = e.target;
-    let error = false;
-    let success = true;
-
-    if (root.title.value && root.description.value && root.name.value && root.loc.value && root.price.value && root.email.value) {
-        
-        if (!isNaN(parseFloat(root.price.value)) || root.price.value==0) {
-            let data = {title:root.title.value,
-                        description:root.description.value,
-                        name:root.name.value,
-                        loc:root.loc.value,
-                        price:root.price.value,
-                        vb:(root.vb.checked == true)?1:0,
-                        email:root.email.value};
-                     
-                connect_to_api(url+"add", "POST", JSON.stringify(data)).catch(console.log);
-                connect_to_api(url+"show", "GET").then(e=>create_items(e,"default"));
-                form.trigger("reset");
-                success = "Your entry has been successfully added!";
-                div_entry.toggleClass( "is-active" );
-        }
-        else
-            error = "Price must be in the format xx.xx or 0!";    
-    }
-    else
-        error = "Please fill out all fields!";
-
-    if (error) {
-        div_message.toggleClass( "is-active" );
-        lastclass = "is-danger";
-        div_message.find(".message-header p").append("Error!");
-        div_message.find(".message").addClass(lastclass);
-        div_message.find(".message-body").append(error);
-    }
-    else {
-        div_message.toggleClass( "is-active" );
-        lastclass = "is-primary";
-        div_message.find(".message-header p").append("Success!");
-        div_message.find(".message").addClass(lastclass);
-        div_message.find(".message-body").append(success);
-    }
-})
-
-btn_close_itemfull.on("click",  function(){div_itemfull.toggleClass( "is-active" )});
-btn_new_entry.on("click",  function(){div_entry.toggleClass( "is-active" )});
-btn_close_entry.on("click",  function(){div_entry.toggleClass( "is-active" ); form.trigger("reset")});
-btn_form_reset.on("click",  ()=>form.trigger("reset"));
-inp_search_loc.on("keyup",function(){create_matches(this.value)});
-inp_search_loc.on("click",()=>inp_search_loc.val(""));
-
-ul_matches.on("click",e=>{
-    if(e.target.tagName==="A"){
-        inp_search_loc.val(e.target.text);
-        ul_matches.empty();
-        connect_to_api(url+"search/loc/"+e.target.text, "GET").then(e=>create_items(e,"search"));        
-    }    
+btn_search_submit.on("click", () => {
+  if (inp_search.val()) {
+    connect_to_api(url + "search/" + inp_search.val(), "GET").then((e) =>
+      create_items(e, "search")
+    );
+  }
 });
 
-btn_close_message.on("click",  function(){
-    div_message.toggleClass( "is-active" );
-    div_message.find(".message-header p").empty();
-    div_message.find(".message").removeClass(lastclass);
-    div_message.find(".message-body").empty();
-}); 
+form.on("submit", (e) => {
+  e.preventDefault();
+  let root = e.target;
+  let error = false;
+  let success = true;
+
+  if (
+    root.title.value &&
+    root.description.value &&
+    root.name.value &&
+    root.loc.value &&
+    root.price.value &&
+    root.email.value
+  ) {
+    if (!isNaN(parseFloat(root.price.value)) || root.price.value == 0) {
+      let data = {
+        title: root.title.value,
+        description: root.description.value,
+        name: root.name.value,
+        loc: root.loc.value,
+        price: root.price.value,
+        vb: root.vb.checked == true ? 1 : 0,
+        email: root.email.value,
+      };
+
+      connect_to_api(url + "add", "POST", JSON.stringify(data)).catch(
+        console.log
+      );
+      connect_to_api(url + "show", "GET").then((e) =>
+        create_items(e, "default")
+      );
+      form.trigger("reset");
+      success = "Your entry has been successfully added!";
+      div_entry.toggleClass("is-active");
+    } else error = "Price must be in the format xx.xx or 0!";
+  } else error = "Please fill out all fields!";
+
+  if (error) {
+    div_message.toggleClass("is-active");
+    lastclass = "is-danger";
+    div_message.find(".message-header p").append("Error!");
+    div_message.find(".message").addClass(lastclass);
+    div_message.find(".message-body").append(error);
+  } else {
+    div_message.toggleClass("is-active");
+    lastclass = "is-primary";
+    div_message.find(".message-header p").append("Success!");
+    div_message.find(".message").addClass(lastclass);
+    div_message.find(".message-body").append(success);
+  }
+});
+
+btn_close_itemfull.on("click", function () {
+  div_itemfull.toggleClass("is-active");
+});
+btn_new_entry.on("click", function () {
+  div_entry.toggleClass("is-active");
+});
+btn_close_entry.on("click", function () {
+  div_entry.toggleClass("is-active");
+  form.trigger("reset");
+});
+btn_form_reset.on("click", () => form.trigger("reset"));
+inp_search_loc.on("keyup", function () {
+  create_matches(this.value);
+});
+inp_search_loc.on("click", () => inp_search_loc.val(""));
+
+ul_matches.on("click", (e) => {
+  if (e.target.tagName === "A") {
+    inp_search_loc.val(e.target.text);
+    ul_matches.empty();
+    connect_to_api(url + "search/loc/" + e.target.text, "GET").then((e) =>
+      create_items(e, "search")
+    );
+  }
+});
+
+btn_close_message.on("click", function () {
+  div_message.toggleClass("is-active");
+  div_message.find(".message-header p").empty();
+  div_message.find(".message").removeClass(lastclass);
+  div_message.find(".message-body").empty();
+});
